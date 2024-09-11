cf6a4aaf3980616e327b958793bb5475c3886f59
cbcaetanoborges <cbcaetanoborges@gmail.com>
2024-09-11 16:35:22 +0100

    iou

---
+++ ativarPush.php
@@ -0,0 +1,16 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+include("Classes/Funcoes.php");
+
+
+
+    $query = Funcoes::conexao()->prepare("INSERT INTO push (usuario, authtoken, p256dh, endpoint) VALUES (?, ?, ?, ?)");
+    $query->bindValue(1,$_POST['restaurante']);
+    $query->bindValue(2,$_POST['auth']);
+    $query->bindValue(3,$_POST['pdh']);
+    $query->bindValue(4,$_POST['endpoint']);
+    $query->execute();
+
+//var_dump($_POST);
